01c364f212b0115f52d4eeb854d7d9b33e61a68a
astr0 <68482847+vxcute@users.noreply.github.com>
2021-04-17 04:19:51 +0200

    Add files via upload

---
+++ runsdvui.cmd
@@ -0,0 +1,2 @@
+cd /d "C:\Users\e\Desktop\parcord" &msbuild "paracordbot.csproj" /t:sdvViewer /p:configuration="Release" /p:platform="Any CPU" /p:SolutionDir="C:\Users\e\Desktop\parcord" 
+exit %errorlevel% 
