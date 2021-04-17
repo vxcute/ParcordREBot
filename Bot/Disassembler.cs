df355cf91a69edaea9076cec1aa602d948702fa7
astr0 <68482847+vxcute@users.noreply.github.com>
2021-04-17 17:22:32 +0200

    Update Disassembler.cs

--- Bot/Disassembler.cs
+++ Bot/Disassembler.cs
@@ -20,67 +20,8 @@ namespace paracordbot.Bot.Disassmbler
                 return instructions;
             }
         }
-
-        public X86Instruction[] Disasmx86(byte[] bytes)
-        {
-            const X86DisassembleMode _x86DisassembleMode = X86DisassembleMode.Bit32;
-
-            using (CapstoneX86Disassembler disassembler = CapstoneX86Disassembler.CreateX86Disassembler(_x86DisassembleMode))
-            {
-                disassembler.EnableInstructionDetails = true;
-                disassembler.DisassembleSyntax = DisassembleSyntax.Intel;
-                X86Instruction[] instructions = disassembler.Disassemble(bytes);
-                return instructions;
-            }
-        }
-
-        public MipsInstruction[] Disasmips32(byte[] bytes)
-        {
-
-            const MipsDisassembleMode mipsmode = MipsDisassembleMode.Bit32;
-            using (CapstoneMipsDisassembler disassembler = CapstoneX86Disassembler.CreateMipsDisassembler(mipsmode))
-            {
-                disassembler.EnableInstructionDetails = true;
-                disassembler.DisassembleSyntax = DisassembleSyntax.Intel;
-                MipsInstruction[] instructions = disassembler.Disassemble(bytes);
-                return instructions;
-            }
-        }
-
-        public MipsInstruction[] Disasmips64(byte[] bytes)
-        {
-
-            const MipsDisassembleMode mipsmode = MipsDisassembleMode.Bit64;
-            using (CapstoneMipsDisassembler disassembler = CapstoneX86Disassembler.CreateMipsDisassembler(mipsmode))
-            {
-                disassembler.EnableInstructionDetails = true;
-                disassembler.DisassembleSyntax = DisassembleSyntax.Intel;
-                MipsInstruction[] instructions = disassembler.Disassemble(bytes);
-                return instructions;
-            }
-        }
-
-
-        public X86Instruction[] Disasmx64(byte[] bytes)
-        {
-            const X86DisassembleMode _x86DisassembleMode = X86DisassembleMode.Bit64;
-
-            using (CapstoneX86Disassembler disassembler = CapstoneX86Disassembler.CreateX86Disassembler(_x86DisassembleMode))
-            {
-                disassembler.EnableInstructionDetails = true;
-                disassembler.DisassembleSyntax = DisassembleSyntax.Intel;
-                X86Instruction[] instructions = disassembler.Disassemble(bytes);
-                return instructions;
-            }
-        }
-
-        public static byte[] HexStringToByteArray(string hex)
-        {
-            return Enumerable.Range(0, hex.Length)
-                             .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                             .ToArray();
-        }
     }
 }
+
+
 
