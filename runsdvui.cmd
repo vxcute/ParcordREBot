3032c7f55288369f5dc305de7e2dcf5e4bf92eda
astr0 <68482847+vxcute@users.noreply.github.com>
2021-05-29 08:03:12 +0200

    Delete runsdvui.cmd

--- runsdvui.cmd
+++
@@ -1,2 +0,0 @@
-cd /d "C:\Users\e\Desktop\parcord" &msbuild "paracordbot.csproj" /t:sdvViewer /p:configuration="Release" /p:platform="Any CPU" /p:SolutionDir="C:\Users\e\Desktop\parcord" 
-exit %errorlevel% 
